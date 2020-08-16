f9ffe85ace79bb1345c7ca763418fcd68779abe1
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-08-15 21:35:23 -0400

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # ngnix configuration  with puppet
-node {'nginx':
-	$myvar = $file="/etc/nginx/sites-enabled/default",
-}
-exec {'http_header':
-command  => 'sudo apt-get -y update && 
-             sudo apt-get -y install nginx && 
-             sudo sed -i "30i \\\tadd_header X-Served-By \$hostname;\n" $file  &&
-             sudo service nginx restart',
-provider => shell,
+class node {
+	$myvar = $file="/etc/nginx/sites-enabled/default"
+
+	exec {'http_header':
+	command  => 'sudo apt-get -y update && 
+	             sudo apt-get -y install nginx && 
+	             sudo sed -i "30i \\\tadd_header X-Served-By \$hostname;\n" $file  &&
+	             sudo service nginx restart',
+	provider => shell,
 }
