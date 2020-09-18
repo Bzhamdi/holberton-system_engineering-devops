28c0def79d8190105d5111e9b9bae916063c846a
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-09-17 21:19:26 -0400

    api top 10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+ueries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """get the numbers"""
+    header = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0)'
+    pep = ' Gecko/20100101 Firefox/80.0'
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={
+            'User-Agent': header + pep}, params={"limit": 10}).json()
+    try:
+
+        for x in response["data"]["children"]:
+            print(x["data"]["title"])
+    except KeyError:
+        return 0
