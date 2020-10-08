5b267d80fa303481cf2f5ba3678eea7444a24ae4
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-10-08 08:18:27 -0400

    user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # session failure chane user limit from 4 and 5 to 1000 and the max is 65535                                                            
-exec {'hard':                                                                                                                           
-path     => ['/usr/bin/'],                                                                                                             
-command  => "sudo sed -i 's/holberton hard nofile 5/holberton hard nofile 1000/g' /etc/security/limits.conf",                           
-provider => 'shell',                                                                                                                    
-}                                                                                                                                       
-exec {'soft':                                                                                                                           
-path     => ['/usr/bin/'],                                                                                                             
-command  => "sudo sed -i 's/holberton soft nofile 4/holberton soft nofile 1000/g' /etc/security/limits.conf",                           
-provider => 'shell',                                                                                                                    
+exec {'hard':
+path     => ['/usr/bin/'],
+command  => "sudo sed -i 's/holberton hard nofile 5/holberton hard nofile 1000/g' /etc/security/limits.conf",
+provider => 'shell',
+}
+exec {'soft':
+path     => ['/usr/bin/'],
+command  => "sudo sed -i 's/holberton soft nofile 4/holberton soft nofile 1000/g' /etc/security/limits.conf",
+provider => 'shell',
 }
